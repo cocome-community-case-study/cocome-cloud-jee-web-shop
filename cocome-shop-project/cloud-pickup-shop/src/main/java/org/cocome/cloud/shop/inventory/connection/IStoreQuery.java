114594c056866ead1da955f3fa38088a01493482
Rudolf Biczok <rcbiczok@gmail.com>
2017-12-03 16:47:27 +0100

    Remove warnings

--- cocome-shop-project/cloud-pickup-shop/src/main/java/org/cocome/cloud/shop/inventory/connection/IStoreQuery.java
+++ cocome-shop-project/cloud-pickup-shop/src/main/java/org/cocome/cloud/shop/inventory/connection/IStoreQuery.java
@@ -1,7 +1,5 @@
 package org.cocome.cloud.shop.inventory.connection;
 
-import java.util.List;
-
 import org.cocome.cloud.logic.stub.NotInDatabaseException_Exception;
 import org.cocome.cloud.logic.stub.ProductOutOfStockException_Exception;
 import org.cocome.cloud.logic.stub.UpdateException_Exception;
@@ -9,20 +7,22 @@ import org.cocome.cloud.shop.inventory.store.ProductWrapper;
 import org.cocome.cloud.shop.inventory.store.Store;
 import org.cocome.cloud.shop.shoppingcart.IShoppingCart;
 
+import java.util.List;
+
 /**
  * Interface to retrieve stock items from a specific store and to account sales
  * at that store.
- * 
+ *
  * @author Tobias Pöppke
  * @author Robert Heinrich
  */
 public interface IStoreQuery {
-	public List<ProductWrapper> queryStockItems(Store store) throws NotInDatabaseException_Exception;
+    List<ProductWrapper> queryStockItems(Store store) throws NotInDatabaseException_Exception;
 
-	public ProductWrapper getStockItemByProductID(Store store, long productID);
+    ProductWrapper getStockItemByProductID(Store store, long productID);
 
-	public ProductWrapper getStockItemByBarcode(Store store, long barcode);
+    ProductWrapper getStockItemByBarcode(Store store, long barcode);
 
-	public boolean accountSale(IShoppingCart cart)
-			throws NotInDatabaseException_Exception, ProductOutOfStockException_Exception, UpdateException_Exception;
+    boolean accountSale(IShoppingCart cart)
+            throws NotInDatabaseException_Exception, ProductOutOfStockException_Exception, UpdateException_Exception;
 }
